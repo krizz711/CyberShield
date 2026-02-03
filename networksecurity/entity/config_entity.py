ccb21e497db23b2068ca2b2fc60af65da3780b93
Krizz711 <munalmax777@gmail.com>
2026-02-03 16:12:03 +0530

    data validation completed

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -41,4 +41,20 @@ class DataIngestionConfig:
         self.train_test_split_ratio:float=training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION 
         
         
+
+
+class  DataValidationConfig:
+    def __init__(self,traning_piepline_config:TrainingPipelineConfig):
+        self.data_validation_dir: str =os.path.join(traning_piepline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAME)
+        self.valid_data_dir: str=os.path.join(self.data_validation_dir,training_pipeline.DATA_VALIDATION_VALID_DIR)
+        self.invalid_data_dir: str=os.path.join(self.data_validation_dir,training_pipeline.DATA_VALIDATION_INVALID_DIR)
+        self.valid_train_file_path: str=os.path.join(self.valid_data_dir,training_pipeline.TRAIN_FILE_NAME)
+        self.valid_test_file_path: str=os.path.join(self.valid_data_dir,training_pipeline.TEST_FILE_NAME)
+        self.invalid_train_file_path: str=os.path.join(self.invalid_data_dir,training_pipeline.TRAIN_FILE_NAME)
+        self.invalid_test_file_path: str=os.path.join(self.invalid_data_dir,training_pipeline.TEST_FILE_NAME)
+        self.drift_report_file_path: str = os.path.join(
+            self.data_validation_dir,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME
+        )
         

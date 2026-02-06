8b97f37530004b0dfcfd2931a65b5cd5011a59ee
Krizz711 <munalmax777@gmail.com>
2026-02-06 16:26:12 +0530

    traning model build

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -95,7 +95,8 @@ class DataTransformation:
                 transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
                 transformed_test_file_path=self.data_transformation_config.transformed_test_file_path 
             )
-             
+            
+            return data_transformation_artifact
             
             
             
